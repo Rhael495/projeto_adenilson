5740a1265eb68c0d463deac274c7b34aa48dafe4
rhael <jpjrhae@gmail.com>
2024-04-19 15:06:49 -0300

    versão 1

---
+++ adenilson.js
@@ -0,0 +1,35 @@
+var header = document.getElementById('header');
+var navigationHeader = document.getElementById('navigation_header');
+var content = document.getElementById('content');
+var showSidebar = false;
+
+function toggleSidebar()
+{
+    showSidebar = !showSidebar;  
+    if(showSidebar)
+    {
+        navigationHeader.style.marginLeft = '-10vw';
+        navigationHeader.style.animationName = 'sidebar';
+        content.style.filter = 'blur(2px)';
+    }
+    else
+    {
+        navigationHeader.style.marginLeft = '-100vw';
+        navigationHeader.style.animationName = ''
+        content.style.filter = '';
+    }
+}
+function closeSidebar()
+{
+    if(showSidebar)
+    {
+        toggleSidebar();
+    }
+
+}
+window.addEventListener('resize', function(event){
+    if(window.innerWidth > 880 && showSidebar)
+    {
+        toggleSidebar();
+    }
+});
